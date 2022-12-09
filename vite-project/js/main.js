7f5415738bfdc7f956b5a5633f08ee979977f278
Tommy <ytommy0111@aol.com>
2022-12-09 10:16:22 -0500

    updated arrays and fixed somes tuff

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,84 +1,50 @@
-import "./style";
+import "../styles/style.css";
 import "./seals";
-import javascriptLogo from "./javascript.svg";
 import { setupCounter } from "./counter.js";
 import { seals } from "./seals";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+console.log("======================Expensive Seals======================");
+seals
+  .filter((seal) => seal.value >= 2500)
+  .forEach((seal) => console.log(seal.names));
 
-function value1(expensive) {
-  seals
-    .filter((seal) => seal.price >= 2500)
-    .forEach((seal) => console.log(seal.value))
-     console.log(expensive)
-}
+console.log("======================Cheap Seals======================");
 
-function value2(cheap) {
-  seals
-    .filter((seal) => seal.price < 2500)
-    .forEach((seal) => console.log(seal.value));
-     console.log(cheap)
-}
+seals
+  .filter((seal) => seal.value < 2500)
+  .forEach((seal) => console.log(seal.names));
 
-insertfunctionname {
-  seals
-    .filter((seal) => seal.age >= 13)
-    .forEach((seal) => console.log(seal.age));
-}
+console.log("=======================Older Seals=====================");
+seals
+  .filter((seal) => seal.age >= 13)
+  .forEach((seal) => console.log(seal.names));
 
-insertfunctionname {
-  seals
-    .filter((seal) => seal.age < 13)
-    .forEach((seal) => console.log(seal.age));
-}
+console.log("=======================Young Seals=====================");
+seals
+  .filter((seal) => seal.age < 13)
+  .forEach((seal) => console.log(seal.names));
 
-insertfunctionname {
-  seals
-    .filter((seal) => seal.age < 13)
-    .forEach((seal) => console.log(seal.age));
-}
+console.log("=======================Skinny Seals=====================");
+seals
+  .filter((seal) => seal.weight < 300)
+  .forEach((seal) => console.log(seal.names));
 
-insertfunctionname {
-  seals
-    .filter((seal) => seal.weight < 300)
-    .forEach((seal) => console.log(seal.weight));
-}
+console.log("=======================Fat Seals=====================");
+seals
+  .filter((seal) => seal.weight >= 300)
+  .forEach((seal) => console.log(seal.names));
 
-insertfunctionname {
-  seals
-    .filter((seal) => seal.weight >= 300)
-    .forEach((seal) => console.log(seal.weight));
-}
+console.log("=======================Angry Seals=====================");
+seals
+  .filter((seal) => seal.hostile === "yes")
+  .forEach((seal) => console.log(seal.names));
 
-insertfunctionname {
-  seals
-    .filter((seal) => seal.hostile === "yes")
-    .forEach((seal) => console.log(seal.hostile));
-}
-
-insertfunctionname {
-  seals
-    .filter((seal) => seal.hostile === "no")
-    .forEach((seal) => console.log(seal.hostile));
-}
-
-const hamburger = seal.weight.map((yumme) => yumme * 1.5);
+console.log("=======================Friendly Seals=====================");
+seals
+  .filter((seal) => seal.hostile === "no")
+  .forEach((seal) => console.log(seal.names));
+console.log("=======================After Burger Seals=====================");
+const hamburger = seals.weight.map((yumme) => yumme * 1.5);
 console.log(hamburger);
 
 setupCounter(document.querySelector("#counter"));
